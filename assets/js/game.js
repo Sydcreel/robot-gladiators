7550fd0990092ddbca862dd4e48d6ecc2b2d461b
unknown <Sydney Creel>
2020-10-22 00:34:29 -0500

    edit for loop to loop robots

--- assets/js/game.js
+++ assets/js/game.js
@@ -15,7 +15,9 @@ var enemyAttack = 12;
 
 // create fight function
 var fight = function(enemyName) {
-    window.alert("Welcome to Robot Glaiators!");
+
+// repeat and execute as long as the enemy-robot is alive
+while(enemyHealth > 0) {
 
 var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
@@ -56,7 +58,7 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
     // if yes (true), leave fight
     if (confirmSkip) {
       window.alert(playerName + " has decided to skip this fight. Goodbye!");
-      
+
       // subtract money from playerMoney for skipping
       playerMoney = playerMoney - 2;
     }
@@ -67,9 +69,12 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
   } else {
     window.alert("You need to choose a valid option. Try again!");
   }
+}
 };
 
 // execute fight function
 for(var i = 0; i < enemyNames.length; i++) {
+  var pickedEnemyName = enemyNames[i];
+  enemyHealth = 50;
     fight(enemyNames[i]);
   }
